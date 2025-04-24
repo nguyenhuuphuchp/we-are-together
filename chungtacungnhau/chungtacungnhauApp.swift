37b2c415f5b39cd865f4ff57e4238a053e35fc60
nguyenhuuphuc <phucnh61@fpt.com>
2025-04-24 21:21:52 +0700

    Initial Commit

---
+++ chungtacungnhau/chungtacungnhauApp.swift
@@ -0,0 +1,17 @@
+//
+//  chungtacungnhauApp.swift
+//  chungtacungnhau
+//
+//  Created by Nguyễn Hữu Phúc on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct chungtacungnhauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chungtacungnhauTests/chungtacungnhauTests.swift
@@ -0,0 +1,17 @@
+//
+//  chungtacungnhauTests.swift
+//  chungtacungnhauTests
+//
+//  Created by Nguyễn Hữu Phúc on 24/4/25.
+//
+
+import Testing
+@testable import chungtacungnhau
+
+struct chungtacungnhauTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
